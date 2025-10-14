edce65324c7fb0c6e922064fc17ab2d4ca352d43
DoH100 <147162323+DoH100@users.noreply.github.com>
2025-10-15 01:11:57 +0900

    [Bronze V] Title: 구구단, Time: 188 ms, Memory: 18652 KB -BaekjoonHub

---
+++ 백준/Bronze/2739. 구구단/구구단.java
@@ -0,0 +1,14 @@
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int a = input.nextInt();
+
+        for (int i = 1; i <= 9; i++) {
+            System.out.println(a + " * " + i + " = " + (a * i));
+        }
+    }
+}
